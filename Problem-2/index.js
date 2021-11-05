b73fb0734e19f6595d6d896053fc8181c3fc5d61
solenedel <solene98@student.ubc.ca>
2021-11-05 14:30:41 -0700

    complete task 1

--- Problem-2/index.js
+++ Problem-2/index.js
@@ -12,14 +12,24 @@ async function main() {
 
   const matilda = await Student.findOne({ name: /matilda/i });
 
-
   // Complete assignment here
 
-  console.log('MATILDA ID: ', matilda.id);
+  // console.log('MATILDA ID: ', matilda.id);
 
-  // 1. Find all Enrollments that Student Matilda has. 
-  // +Print the IDs of the Enrollments and Classes to the console.
+  // TASK 1: 
+  // Find all Enrollments that Student Matilda has. 
+  // Print the IDs of the Enrollments and Classes to the console.
+
   const enrollments = await Enrollment.find({ _studentID: matilda.id });
-  console.log('Matilda\'s enrollments: ', enrollments);
+
+  // parse data returned from query
+  JSON.parse(JSON.stringify(enrollments));
+
+  // iterate through the enrollments array
+  console.log('Matilda\'s enrollments');
+  for (const enrollment of enrollments) {
+    console.log(`Enrollment ID: ${enrollment.id}, Class ID: ${enrollment._classID}`);
+  }
+  
 
 }
