2ab2920ca4736cbdf39b508ba1214d4a6d3d4e0b
solenedel <solene98@student.ubc.ca>
2021-11-04 20:15:35 -0700

    work on problem 2

--- Problem-2/index.js
+++ Problem-2/index.js
@@ -10,8 +10,16 @@ async function main() {
 
   const { Student, Class, Enrollment } = models()
 
-  const matilda = await Student.findOne({ name: /matilda/i })
+  const matilda = await Student.findOne({ name: /matilda/i });
+
+
   // Complete assignment here
 
+  console.log('MATILDA ID: ', matilda.id);
+
+  // 1. Find all Enrollments that Student Matilda has. 
+  // +Print the IDs of the Enrollments and Classes to the console.
+  const enrollments = await Enrollment.find({ _studentID: matilda.id });
+  console.log('Matilda\'s enrollments: ', enrollments);
 
 }
